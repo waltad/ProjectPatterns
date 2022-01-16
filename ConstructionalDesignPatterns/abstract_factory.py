b835eac92a608c1a211f50444463c2fec92839e5
Tadek <tadek.waluga@gmail.com>
2022-01-16 22:35:23 +0100

    Changing file abstract_factory.py

--- ConstructionalDesignPatterns/abstract_factory.py
+++ ConstructionalDesignPatterns/abstract_factory.py
@@ -10,7 +10,42 @@ class Sedan:
 
 class LHDSedan(Sedan):
     def who_am_i(self):
+        print('Left Hand Drive Sedan')
+
+
+class RHDSedan(Sedan):
+    def who_am_i(self):
+        print('Right Hand Drive Sedan')
+
+
+class StationWagon:
+    def who_am_i(self):
         pass
+
+
+class LHDStationWagon(StationWagon):
+    def who_am_i(self):
+        print('Left Hand Drive Station Wagon')
+
+
+class RHDStationWagon(StationWagon):
+    def who_am_i(self):
+        print('Right Hand Drive Station Wagon')
+
+
+class Coupe:
+    def who_am_i(self):
+        pass
+
+
+class LHDCoupe(Coupe):
+    def who_am_i(self):
+        print('Left Hand Drive Coupe')
+
+
+class RHDCoupe(Coupe):
+    def who_am_i(self):
+        print('Right Hand Drive Coupe')
 
 
 class CarFactory:
@@ -26,10 +61,39 @@ class CarFactory:
 
 class LHDCarFactory(CarFactory):
     def create_sedan(self):
-        pass
+        return LHDSedan()
 
     def create_station_wagon(self):
-        pass
+        return LHDStationWagon()
 
     def create_coupe(self):
-        pass
+        return LHDCoupe()
+
+
+class RHDCarFactory(CarFactory):
+    def create_sedan(self):
+        return RHDSedan()
+
+    def create_station_wagon(self):
+        return RHDStationWagon()
+
+    def create_coupe(self):
+        return RHDCoupe()
+
+
+def main():
+    car_factory = LHDCarFactory()
+#    car_factory = RHDCarFactory()
+
+    sedan = car_factory.create_sedan()
+    sedan.who_am_i()
+
+    station_wagon = car_factory.create_station_wagon()
+    station_wagon.who_am_i()
+
+    coupe = car_factory.create_coupe()
+    coupe.who_am_i()
+
+
+if __name__ == '__main__':
+    main()
