1665f7f4d638e0d4398c7c014f837c598b8ae97a
GI10 <tadus@op.pl>
2022-01-19 12:20:38 +0100

    Adding files class_adapter.py and class_adapter.py

--- StructuralDesignPatterns/class_adapter.py
+++ StructuralDesignPatterns/class_adapter.py
@@ -1,3 +1,18 @@
+"""Zadanie 1 - Adapter
+Korzystając z wzorca Adapter napisz wrapper na listę tak, żeby interfejs dla klienta składał się z metod push, pop
+i is_empty.
+
+Stwórz dwie implementacje:
+opartą o adapter obiektowy - wykorzystaj składanie obiektów
+opartą o adapter klasowy - wykorzystaj wielodziedziczenie do dostosowania interfejsu listy do oczekiwanego interfejsu
+stosu.
+Stworzone implementacje wykorzystaj do obliczenia wyrażeń podanych w odwrotnej notacji polskiej (ONP):
+https://pl.wikipedia.org/wiki/Odwrotna_notacja_polska
+'2 7 + 3 / 14 3 - 4 * + 2 /'
+'12 2 3 4 * 10 5 / + * +'
+'5 1 2 + 4 * + 3 -'"""
+
+
 # adapter klasowy
 class StackInterface:
     def push(self, data):
@@ -12,10 +27,39 @@ class StackInterface:
 
 class Stack(StackInterface, list):
     def push(self, data):
-        pass
+        list.append(self, data)
 
     def pop(self):
-        pass
+        return list.pop(self)
 
     def is_empty(self):
-        pass
+        return not list.__len__(self)
+
+
+def main():
+    expr = '2 7 + 3 / 14 3 - 4 * + 2 /'
+    # expr = '12 2 3 4 * 10 5 / + * +'
+    # expr = '5 1 2 + 4 * + 3 -'
+    stack = Stack()
+
+    for el in expr.split():
+        if el.isnumeric():
+            stack.push(float(el))
+        else:
+            a = stack.pop()
+            b = stack.pop()
+            if el == '+':
+                stack.push(a + b)
+            elif el == '-':
+                stack.push(b - a)
+            elif el == '*':
+                stack.push(a * b)
+            else:
+                stack.push(b / a)
+
+    print(stack.pop())
+    print(stack.is_empty())
+
+
+if __name__ == '__main__':
+    main()
