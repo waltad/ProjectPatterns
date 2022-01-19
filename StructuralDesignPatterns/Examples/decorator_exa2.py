ad3b958425d09c6f2fcf439dc3100336851cc5b3
GI10 <tadus@op.pl>
2022-01-19 14:53:22 +0100

    Adding files decorator_exa1.py and decorator_exa2.py

---
+++ StructuralDesignPatterns/Examples/decorator_exa2.py
@@ -0,0 +1,28 @@
+import functools
+
+
+def cache(fn):
+    _cache = dict()
+
+    @functools.wraps(fn)
+    def cacher(*args):
+        if args not in _cache:
+            _cache[args] = fn(*args)
+        return _cache[args]
+    return cacher
+
+
+@cache
+def fibonacci(n):
+    if n in (0, 1):
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+def main():
+    fibonacci(35)
+
+
+if __name__ == '__main__':
+    main()
