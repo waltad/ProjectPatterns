801ec4feb6f5a0cc2890ce4467aa303c5a60d582
GI10 <tadus@op.pl>
2022-02-03 16:23:10 +0100

    Changing file command.py

--- BehavioralDesignPatterns/command.py
+++ BehavioralDesignPatterns/command.py
@@ -6,8 +6,12 @@ Uzupełnij program o interakcję z użytkownikiem, który będzie podawał polec
 
 
 class Robot:
-    def __init__(self):
-        pass
+    def __init__(self, pos_x, pos_y):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def __str__(self):
+        return f"({self.pos.x}, {self.pos_y})"
 
 
 class Command:
@@ -20,24 +24,67 @@ class Command:
 
 class MoveUp(Command):
     def __init__(self, steps):
-        pass
+        self.steps = steps
 
     def execute(self, robot):
-        pass
+        robot.pos_y += self.steps
 
     def undo(self, robot):
-        pass
+        robot.pos_y -= self.steps
+
+
+class MoveDown(Command):
+    def __init__(self, steps):
+        self.steps = steps
+
+    def execute(self, robot):
+        robot.pos_y -= self.steps
+
+    def undo(self, robot):
+        robot.pos_y += self.steps
+
+
+class MoveRight(Command):
+    def __init__(self, steps):
+        self.steps = steps
+
+    def execute(self, robot):
+        robot.pos_x += self.steps
+
+    def undo(self, robot):
+        robot.pos_x -= self.steps
+
+
+class MoveLeft(Command):
+    def __init__(self, steps):
+        self.steps = steps
+
+    def execute(self, robot):
+        robot.pos_x -= self.steps
+
+    def undo(self, robot):
+        robot.pos_x += self.steps
 
 
 class Commander:
     def __init__(self, robot):
-        pass
+        self._robot = robot
+        self._undo_stack = []
+        self._redo_stack = []
 
     def execute(self, cmd):
-        pass
+        cmd.execute(self._robot)
+        self._undo_stack.append(cmd)
+        self._redo_stack.clear()
 
     def undo(self):
-        pass
+        if self._undo_stack:
+            cmd = self._undo_stack.pop()
+            cmd.undo(self._robot)
+            self._redo_stack.append(cmd)
 
     def redo(self):
-        pass
+        if self._redo_stack:
+            cmd = self._redo_stack
+            cmd.execute(self._robot)
+            self._undo_stack(cmd)
